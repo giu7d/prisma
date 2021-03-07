6176e7d30b3e7347a6228622d9d95b8bac016a7d
Giuseppe Setem <giuseppe7d@gmail.com>
2021-03-06 21:42:22 -0300

    feat(_document): add html head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,11 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
